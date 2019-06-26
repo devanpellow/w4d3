7d47f7655fecab92ee8536e065a1f33784bdd6ed
Devan Pellow <devanpellow@gmail.com>
2019-06-26 12:26:13 +0200

    app.js express

--- express-server/app.js
+++ express-server/app.js
@@ -1,2 +1,14 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+
+const app = require("express")();
+
+app.get("/", (req, res) => {
+	res.send("<h1>Hello world</h1>");
+});
+
+app.get("/about", (req, res) => {
+	res.send("welcome to my website");
+});
+
+app.listen(3000);
